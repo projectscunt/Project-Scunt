c420f02d799b78ebadad142374bef3832f107a2d
Aamir <me@aam1r.com>
2011-01-15 21:26:41 -0500

    Import

--- gen/com/scunt/app/R.java
+++ gen/com/scunt/app/R.java
@@ -12,23 +12,26 @@ public final class R {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
+        public static final int stub=0x7f020001;
     }
     public static final class id {
-        public static final int LinearLayout01=0x7f050009;
-        public static final int ListView01=0x7f05000a;
-        public static final int widget33=0x7f050000;
-        public static final int widget36=0x7f050001;
-        public static final int widget39=0x7f050003;
-        public static final int widget40=0x7f050004;
-        public static final int widget41=0x7f050005;
-        public static final int widget42=0x7f050006;
-        public static final int widget43=0x7f050007;
-        public static final int widget44=0x7f050008;
-        public static final int widget45=0x7f050002;
+        public static final int image=0x7f050000;
+        public static final int list=0x7f05000b;
+        public static final int text=0x7f050001;
+        public static final int widget33=0x7f050002;
+        public static final int widget36=0x7f050003;
+        public static final int widget39=0x7f050005;
+        public static final int widget40=0x7f050006;
+        public static final int widget41=0x7f050007;
+        public static final int widget42=0x7f050008;
+        public static final int widget43=0x7f050009;
+        public static final int widget44=0x7f05000a;
+        public static final int widget45=0x7f050004;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
-        public static final int tasks=0x7f030001;
+        public static final int item=0x7f030000;
+        public static final int main=0x7f030001;
+        public static final int tasks=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
--- src/com/scunt/app/ScuntApp.java
+++ src/com/scunt/app/ScuntApp.java
@@ -1,16 +1,20 @@
 package com.scunt.app;
 
+import java.util.ArrayList;
+
+import com.scunt.app.LazyAdapter;
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.os.Bundle;
-import android.view.KeyEvent;
+import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class ScuntApp extends Activity {
+
+    ListView list;
+    LazyAdapter adapter;
+    private static final String TAG = "AamirDebugging";
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,24 +23,30 @@ public class ScuntApp extends Activity {
     }
         
     // Called when the "Tasks" button is clicked on the front page
-    public void tasksClicked(View view) {
-    	final ListView listView;
-    	String tasksLists[]={"Roof a building", "Picture with a goose", "Go swimming", "Play dodgeball", "Find oldest book", "Hug an older person", "Pump 11 cent gas", "Get a pic inside university club"};
-
-    	setContentView(R.layout.tasks);
-    	listView=(ListView)findViewById(R.id.ListView01);
-    	listView.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_multiple_choice, tasksLists));
-    	listView.setChoiceMode(ListView.CHOICE_MODE_NONE);
-    	listView.setTextFilterEnabled(true);
-    	listView.setOnItemClickListener(new OnItemClickListener() {
-			public void onItemClick(AdapterView<?> a, View v, int position, long id) {
-				AlertDialog.Builder adb=new AlertDialog.Builder(ScuntApp.this);
-				adb.setTitle("Task Status");
-				adb.setMessage("You haven't done this task yet, get working!");
-				adb.setPositiveButton("Ok", null);
-				adb.show();
-			}
-		});
-    	
+    public void tasksClicked(View view) {	
+        setContentView(R.layout.tasks);
+        
+    	// Code goes here        
+        Log.v(TAG, "You clicked on the Tasks button");
+        list=(ListView)findViewById(R.id.list);
+        adapter=new LazyAdapter(this, mStrings);
+        list.setAdapter(adapter);        
     }
+    
+    private ArrayList<String, String> mStrings = new ArrayList<String, String>(); 
+    /*
+    private String[] mStrings={
+            "http://a3.twimg.com/profile_images/670625317/aam-logo-v3-twitter.png",
+            "http://a3.twimg.com/profile_images/740897825/AndroidCast-350_normal.png",
+            "http://a3.twimg.com/profile_images/121630227/Droid_normal.jpg",
+            "http://a1.twimg.com/profile_images/957149154/twitterhalf_normal.jpg",
+            "http://a1.twimg.com/profile_images/97470808/icon_normal.png",
+            "http://a3.twimg.com/profile_images/511790713/AG.png",
+            "http://a3.twimg.com/profile_images/956404323/androinica-avatar_normal.png",
+            "http://a1.twimg.com/profile_images/909231146/Android_Biz_Man_normal.png",
+            "http://a3.twimg.com/profile_images/72774055/AndroidHomme-LOGO_normal.jpg",
+            "http://a1.twimg.com/profile_images/349012784/android_logo_small_normal.jpg",
+            "http://a1.twimg.com/profile_images/841338368/ea-twitter-icon.png",
+            "http://a3.twimg.com/profile_images/77641093/AndroidPlanet_normal.png"
+    };*/
 }
